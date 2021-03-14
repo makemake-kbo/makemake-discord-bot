141e38bcd75fdfe25c965cb6f687896a367ded04
Vukasin Gostovic <vukasin@gostovic.de>
2021-03-14 20:03:00 +0100

    a

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const { Client, MessageAttachment } = require('discord.js');
 const client = new Discord.Client();
-var token = '';
+var token = 'ODIwNzA5OTc1NTc5Njg4OTg3.YE5Hug.oOdbF_Uaoz_NJv3fYVffCEuR1Zo';
 var divisions = ['open', 'low', 'mid', 'high', 'prem'];
 
 client.on('ready', () => {
@@ -22,12 +22,22 @@ client.on('message', msg => {
 			msg.reply('\nyea i watch anime\nfuck yo trollgang\nyou suck my dick\nthis is NOT tiktok homie\nthis aint tiktok blood\nu love me, i love you too\nu a troll, then u can get deez nuts\nu a troll, u can get deez balls in your mouf')
 			break;
 		case '!ask':
-			let attachment2 = new MessageAttachment('https://media.discordapp.net/attachments/760549057134329891/820729066663575562/tenor.gif');
+			let attachment2 = new MessageAttachment('https://media.discordapp.net/attachments/399408989684891651/759183468008701993/image0-2.gif');
 			msg.channel.send(attachment2);
 			break;
 		case '!cry':
-			let attachment3 = new MessageAttachment('https://media.discordapp.net/attachments/760549057134329891/820729066663575562/tenor.gif');
+			let attachment3 = new MessageAttachment('https://media1.tenor.com/images/9ac53890cf2c67689106faf8ec6559c6/tenor.gif?itemid=20298156');
 			msg.channel.send(attachment3);
+			break;
+		case '!mge':
+			let attachment4 = new MessageAttachment('https://images-ext-1.discordapp.net/external/Ss7n5GmKxAJFC7FQzRFO_a2baCZwp_EBLOILjcibrNY/https/media.discordapp.net/attachments/815275671873388568/817099991851204688/caption.gif');
+			msg.channel.send(attachment4);
+			break;
+		case '!anyblacks':
+			msg.reply(' wait. Are you a racist? No, no that cant be! Its not too late to reconsider your life choices and judge people by their charachter, rather than their skin color.');
+
+		case '!commands':
+			msg.reply(' !saturday, !endrit, !anime, !ask, !cry, !mge, !div, !pfp')
 			break;
 		}
 
